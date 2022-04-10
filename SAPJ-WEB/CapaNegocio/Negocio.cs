c4f9ae9da96823f6b53eb382c1675f46476259d5
Patricio González <Razer81368@gmail.com>
2022-04-10 14:17:51 -0400

    Creacion capa negocio y conexion
    Co-Authored-By: Francisco <92338971+EstimadoMaruchan@users.noreply.github.com>
    Co-Authored-By: Luis Cordova <91169088+LuchitoCordova@users.noreply.github.com>

---
+++ SAPJ-WEB/CapaNegocio/Negocio.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaConexion;
+
+namespace CapaNegocio
+{
+    public class Negocio
+    {
+        private ConexionSQL conec;
+
+        public ConexionSQL Conec { get => conec; set => conec = value; }
+
+        public void ConexionSQL()
+        {
+            this.Conec = new ConexionSQL();
+            this.Conec.NombreTabla = "Usuario";
+            this.Conec.NombreBaseDatos = "portafolio";
+            this.Conec.CadenaConexion = @"Data Source=DESKTOP-398JQJ0\SQLEXPRESS;Initial Catalog=portafolio ;Integrated Security=True"; ;
+        }
+    }
+}
